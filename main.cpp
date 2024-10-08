7d012d4b7b8f99087ea4fce181a74bfd07f92e85
JordanEmme <jordanemme@protonmail.com>
2024-10-08 09:45:24 +0100

    Follow SDL2 official doc advice on inclusion

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_filesystem.h>
-#include <SDL2/SDL_keyboard.h>
-#include <SDL2/SDL_log.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_scancode.h>
-#include <SDL2/SDL_stdinc.h>
-#include <SDL2/SDL_timer.h>
-#include <SDL2/SDL_video.h>
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_events.h"
+#include "SDL2/SDL_filesystem.h"
+#include "SDL2/SDL_keyboard.h"
+#include "SDL2/SDL_log.h"
+#include "SDL2/SDL_render.h"
+#include "SDL2/SDL_scancode.h"
+#include "SDL2/SDL_stdinc.h"
+#include "SDL2/SDL_video.h"
 #include <cstring>
 #include <fstream>
 
@@ -399,5 +398,6 @@ int main(int argc, char** argv) {
         update_timers();
         update_display();
     }
+    SDL_Quit();
     return 0;
 }
